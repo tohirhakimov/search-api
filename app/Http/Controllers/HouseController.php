a5bc73dc54991a8fe550fc52459522774e485268
Tohir <hakimovtohirbek@gmail.com>
2022-10-12 11:05:53 +0500

    add controller and resource

---
+++ app/Http/Controllers/HouseController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\House;
+use Illuminate\Http\Request;
+
+
+
+class HouseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        
+    }
+}
